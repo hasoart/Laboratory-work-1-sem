25a26c1c9cc2f1e61ae538007ee74d6c4325928a
Senpai Kirigaia <71059114+SenpaiKirigaia@users.noreply.github.com>
2021-09-26 11:01:02 +0300

    Add files via upload

---
+++ Turtle10.py
@@ -0,0 +1,10 @@
+import turtle as t
+a = 10
+for i in range(3):
+	for i in range(36):
+		t.forward(a)
+		t.left(10)
+	for i in range(36):
+		t.forward(a)
+		t.right(10)
+	t.left(60)
---
+++ Turtle4.py
@@ -0,0 +1,6 @@
+import turtle
+
+turtle.shape('turtle')
+for i in range (359):
+	turtle.forward(1)
+	turtle.left(1)
