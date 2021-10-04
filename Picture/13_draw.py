ce6f749f642e44d855ef645c478271f3a175d550
hasoart <artash.gyolecyan@gmail.com>
2021-10-04 11:59:38 +0300

    Animated hedgehogs

--- Picture/13_draw.py
+++ Picture/13_draw.py
@@ -10,24 +10,39 @@ clock = pygame.time.Clock()
 finished = False
 
 FPS = 60
-WINDOW_SIZE = (400, 600)
+dt = 1/60
+
+WINDOW_SIZE = (800, 1200)
 window = pygame.display.set_mode(WINDOW_SIZE)
 sky = pygame.Surface((800, 670))
 
-screen = pygame.Surface((800, 1200))
+SCREEN_SIZE = (800, 1200)
+screen = pygame.Surface(SCREEN_SIZE)
 sky.fill(colors.sky_color)
 
 ground = pygame.Surface((800, 730))
 ground.fill(colors.ground_color)
 
+hedgehog_count = 3
+hedgehog_x = [350, 250, 10]
+hedgehog_y = [752, 600, 900]
+hedgehog_scale = [1, 0.5, 0.5]
+hedgehog_rotate = [0, 10, -10]
+hedgehog_orientation = [False, False, False]
+hedgehog_velocity = [100, 250, 200]
+
 while not finished:
-    
+
     screen.blit(sky, (0, 0))
     screen.blit(ground, (0, 670))
 
-    dr_obj.draw_object(dr_obj.get_hedgehog(), screen, 350, 752, 1)
-    dr_obj.draw_object(dr_obj.hedgehog, screen, 250, 600, 0.5, 10)
-    dr_obj.draw_object(dr_obj.hedgehog, screen, 10, 900, 0.5, -10)
+    for i in range(hedgehog_count):
+        dr_obj.draw_object(dr_obj.hedgehog, screen, hedgehog_x[i], hedgehog_y[i],
+                           hedgehog_scale[i], hedgehog_rotate[i], hedgehog_orientation[i])
+        hedgehog_x[i] += hedgehog_velocity[i] * dt
+        if hedgehog_x[i] <= 0 or hedgehog_x[i] >= SCREEN_SIZE[0]:
+            hedgehog_velocity[i] *= -1
+            hedgehog_orientation[i] = not hedgehog_orientation[i]
 
     dr_obj.draw_object(dr_obj.mushroom, screen, 700, 1000, 1/3)
     dr_obj.draw_object(dr_obj.mushroom, screen, 750, 990, 0.5, 9)
