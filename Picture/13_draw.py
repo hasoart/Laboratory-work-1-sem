256e717388b995ec52200c9b6bd6656027f1e0d4
hasoart <artash.gyolecyan@gmail.com>
2021-10-04 11:37:51 +0300

    Refactored code

--- Picture/13_draw.py
+++ Picture/13_draw.py
@@ -1,104 +1,50 @@
 import pygame
-from pygame.draw import *
-from random import *
+import pygame.draw as draw
+
+import draw_objects as dr_obj
+import colors
 
 pygame.init()
 
-FPS = 60
-sc = pygame.display.set_mode((800, 1200))
-
-#Фон
-sky = pygame.Surface((800, 670))
-sky.fill((44, 160, 90))
-ground = pygame.Surface((800, 730))
-ground.fill((108, 93, 83))
-sc.blit(sky, (0, 0))
-sc.blit(ground, (0, 670))
-
-#Еж
-#Тело ежика
-ej = pygame.Surface((600, 600), pygame.SRCALPHA)
-ellipse(ej, (72, 55, 55), [0, 150, 40, 20])
-ellipse(ej, (143, 139, 138), [0,150, 40, 20], 2)
-ellipse(ej, (72, 55, 55), [265, 155, 25, 15])
-ellipse(ej, (143, 139, 138), [265, 155, 25, 15], 2)
-ellipse(ej, (72, 55, 55), [10, 100, 270, 90])
-ellipse(ej, (143, 139, 138), [10, 100, 270, 90], 2)
-ellipse(ej, (72, 55, 55), [25, 165, 40, 20])
-ellipse(ej, (143, 139, 138), [25, 165, 40, 20], 2)
-ellipse(ej, (72, 55, 55), [238, 170, 40, 20])
-ellipse(ej, (143, 139, 138), [238, 170, 40, 20], 2)
-ellipse(ej, (72, 55, 55), [244, 122, 72, 40])
-ellipse(ej, (143, 139, 138), [244, 122, 72, 40], 2)
-#Глаза и нос
-circle(ej, (0, 0, 0), (281, 134), 5)
-circle(ej, (143, 139, 138), (281, 134), 5, 1)
-circle(ej, (0, 0, 0), (296, 130), 5)
-circle(ej, (143, 139, 138), (296, 130), 5, 1)
-circle(ej, (0, 0, 0), (315, 138), 3)
-circle(ej, (143, 139, 138), (315, 138), 3, 1)
-
-
-#Мухомор
-
-mh = pygame.Surface((100, 100), pygame.SRCALPHA)
-ellipse(mh, (255, 255, 255), (40, 20, 20, 60))
-ellipse(mh, (255, 0, 0), (20, 10, 60, 20))
-ellipse(mh, (255, 255, 255), (40, 20, 10, 5))
-ellipse(mh, (255, 255, 255), (32, 13, 10, 5))
-ellipse(mh, (255, 255, 255), (60, 15, 10, 5))
-
-
-
-
-#Иглы
-
-ig = pygame.Surface((28, 80), pygame.SRCALPHA)
-
-polygon(ig, (0, 0, 0), ((7, 0), (0, 80), (14, 80)))
-polygon(ig, (120, 39, 70), ((7, 0), (0, 80), (14, 80)), 1)
-polygon(ig, (0, 0, 0), ((21, 0), (14, 80), (28, 80)))
-polygon(ig, (43, 39, 38), ((7, 0), (0, 80), (14, 80)), 1)
-
-for i in range(70):
-    if i == 55:
-        ej.blit(pygame.transform.rotate(mh, 20), (40, 20))
-        ej.blit(pygame.transform.rotate(mh, -10), (130, 25))
-        circle(ej, (200, 170, 10), (40, 70), 30)
-    turn = randint(-20, 40)
-    x = randint(5, 220)
-    y = randint(20, 100)    
-    ej.blit(pygame.transform.rotate(ig, turn), (x, y))
-
-sc.blit(ej, (350, 752))
-
-#еж под прямоугольничком)
-minej = pygame.transform.scale(ej, (ej.get_width()//2, ej.get_height()//2))
-sc.blit(pygame.transform.rotate(minej, 10), (250, 600))
-
-#еж в углу)
-sc.blit(pygame.transform.rotate(minej, -10), (10, 900))
-
-#минимухоморчики
-minimh = pygame.transform.scale(mh, (mh.get_width()//2, mh.get_height()//2))
-miniminimh = pygame.transform.scale(mh, (mh.get_width()//3, mh.get_height()//3))
-
-sc.blit(miniminimh, (700, 1000))
-sc.blit(pygame.transform.rotate(minimh, 9), (750, 990))
-sc.blit(pygame.transform.rotate(minimh, -3), (720, 990))
-sc.blit(pygame.transform.rotate(mh, -7), (620, 950))
-
-#Прямоугольнички? (Чтобы все ежики были под ними)
-rect(sc, (212, 170, 0), (0, 0, 65, 730))
-rect(sc, (212, 170, 0), (102, 0, 205, 800))
-rect(sc, (212, 170, 0), (560, 0, 70, 725))
-rect(sc, (212, 170, 0), (730, 0, 50, 820))
-
-pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
+FPS = 60
+WINDOW_SIZE = (400, 600)
+window = pygame.display.set_mode(WINDOW_SIZE)
+sky = pygame.Surface((800, 670))
+
+screen = pygame.Surface((800, 1200))
+sky.fill(colors.sky_color)
+
+ground = pygame.Surface((800, 730))
+ground.fill(colors.ground_color)
+
 while not finished:
+    
+    screen.blit(sky, (0, 0))
+    screen.blit(ground, (0, 670))
+
+    dr_obj.draw_object(dr_obj.get_hedgehog(), screen, 350, 752, 1)
+    dr_obj.draw_object(dr_obj.hedgehog, screen, 250, 600, 0.5, 10)
+    dr_obj.draw_object(dr_obj.hedgehog, screen, 10, 900, 0.5, -10)
+
+    dr_obj.draw_object(dr_obj.mushroom, screen, 700, 1000, 1/3)
+    dr_obj.draw_object(dr_obj.mushroom, screen, 750, 990, 0.5, 9)
+    dr_obj.draw_object(dr_obj.mushroom, screen, 720, 990, 0.5, -3)
+    dr_obj.draw_object(dr_obj.mushroom, screen, 620, 950, 1, -7)
+
+    pillars = [(0  , 0,  65, 730),
+               (102, 0, 205, 800),
+               (560, 0,  70, 725),
+               (730, 0,  50, 820)]
+    for pillar in pillars:
+        draw.rect(screen, colors.pillar_color, pillar)
+
+    screen_scaled = pygame.transform.scale(screen, WINDOW_SIZE)
+    window.blit(screen_scaled, (0, 0))
+
+    pygame.display.update()
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
---
+++ Picture/draw_objects.py
@@ -0,0 +1,144 @@
+import random
+
+import pygame
+import pygame.draw as draw
+
+import colors
+
+
+def polygon_outlined(surface, colors, coords, width=0):
+    """
+    polygon_outlined(surface, colors, coords, width=0)
+    returns - None
+
+    Draws filled polygon with outline using the coordinates of vertices
+    surface(type - pygame.Surface) - surface to draw on
+    colors(type - tuple:(fill_color, outline_color)) - colors to use
+    coords(list(coordinates) or tuple(coordinates)) - coordinates of polygon vertices
+    width(int, optional) - width of outline in pixels
+    """
+    
+    fill_color, outline_color = colors
+    draw.polygon(surface, fill_color, coords)
+    draw.polygon(surface, outline_color, coords, width)
+
+
+def circle_outlined(surface, colors, center, radius, width=0):
+    """
+    circle_outlined(surface, colors, center, radius, width=0)
+    returns - None
+
+    Draws filled circle with outline using center and radius
+    surface(type - pygame.Surface) - surface to draw on
+    colors(type - tuple:(fill_color, outline_color)) - colors to use
+    center(list(coordinates) or tuple(coordinates)) - coordinates of circle center
+    radius(int or float) - radius of the circle
+    width(int, optional) - width of outline in pixels
+    """
+
+    fill_color, outline_color = colors
+    draw.circle(surface, fill_color, center, radius)
+    draw.circle(surface, outline_color, center, radius, width)
+
+
+def ellpise_outlined(surface, colors, coords, width=0):
+    """
+    ellpise_outlined(surface, colors, coords, width=0)
+    returns - None
+
+    Draws filled ellipse with outline using coordinates of top-left corner, width and height
+    surface(type - pygame.Surface) - surface to draw on
+    colors(type - tuple:(fill_color, outline_color)) - colors to use
+    coords(list(x, y, w, h) or tuple(x, y, w, h)) - (x,y) coordinates of the top-left corner, (w,h) width/height of ellipse
+    width(int, optional) - width of outline in pixels
+    """
+    fill_color, outline_color = colors
+    draw.ellipse(surface, fill_color, coords)
+    draw.ellipse(surface, outline_color, coords, width)
+
+
+def get_mushroom():
+    """
+    get_mushroom(empty)
+    returns - pygame.Surface
+
+    Returns a pygame.Surface with a mushroom drawn on it 
+    """
+
+    mushroom = pygame.Surface((100, 100), pygame.SRCALPHA)
+    draw.ellipse(mushroom, colors.white, (40, 20, 20, 60))
+    draw.ellipse(mushroom, colors.red, (20, 10, 60, 20))
+    draw.ellipse(mushroom, colors.white, (40, 20, 10, 5))
+    draw.ellipse(mushroom, colors.white, (32, 13, 10, 5))
+    draw.ellipse(mushroom, colors.white, (60, 15, 10, 5))
+
+    return mushroom
+
+
+mushroom = get_mushroom()
+
+
+def get_hedgehog():
+    """
+    get_hedgehog(empty)
+    returns - pygame.Surface
+
+    Returns a pygame.Surface with a hedgehog drawn on it 
+    """
+
+    # Body
+    hedgehog = pygame.Surface((600, 600), pygame.SRCALPHA)
+    body_ellipses = [[0  , 150, 40 , 20],
+                     [265, 155, 25 , 15],
+                     [10 , 100, 270, 90],
+                     [25 , 165, 40 , 20],
+                     [238, 170, 40 , 20],
+                     [244, 122, 72 , 40]]
+    for ellipse in body_ellipses:
+        ellpise_outlined(hedgehog, colors.hedgehog_colors, ellipse, 2)
+
+
+    # Eyes and nose
+    circle_outlined(hedgehog, colors.eye_colors, (281, 134), 5, 1)
+    circle_outlined(hedgehog, colors.eye_colors, (296, 130), 5, 1)
+    circle_outlined(hedgehog, colors.eye_colors, (315, 138), 3, 1)
+
+    # Needles
+    needle = pygame.Surface((28, 80), pygame.SRCALPHA)
+    polygon_outlined(needle, colors.needle_colors, ((7, 0), (0, 80), (14, 80)), 1)
+    polygon_outlined(needle, colors.needle_colors, ((21, 0), (14, 80), (28, 80)), 1)
+
+    for i in range(70):
+        if i == 55:
+            hedgehog.blit(pygame.transform.rotate(mushroom, 20), (40, 20))
+            hedgehog.blit(pygame.transform.rotate(mushroom, -10), (130, 25))
+            draw.circle(hedgehog, colors.apple_color, (40, 70), 30)
+        turn = random.randint(-20, 40)
+        x = random.randint(5, 220)
+        y = random.randint(20, 100)    
+        hedgehog.blit(pygame.transform.rotate(needle, turn), (x, y))
+
+    return hedgehog
+
+
+hedgehog = get_hedgehog()
+
+
+def draw_object(obj, surface, x0=0, y0=0, scale=1, rotate=0):
+    """
+    draw_object(obj, surface, x0=0, y0=0, scale=1, rotate=0)
+    returns - None
+
+    blits obj to surface with given trasformations
+
+    obj(pygame.Surface) - surface to be transformed and blitted
+    surface(pygame.Surface) - surface to be blitted on
+    x0(float or int, optional) - x-coordinate if top-left corner of obj relative to surface 
+    y0(float or int, optional) - y-coordinate if top-left corner of obj relative to surface 
+    scale(float or int, optional) - how much obj is scaled before blitting 
+    rotate(float or int, optional) - how much(in degrees) obj is rotated before blitting 
+    """
+
+    object_scaled = pygame.transform.scale(obj, (int(obj.get_width() * scale), int(obj.get_height() * scale)))
+    object_rotated = pygame.transform.rotate(object_scaled, rotate)
+    surface.blit(object_rotated, (x0, y0))
